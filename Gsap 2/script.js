d176de5fdbb7409fbf0b3ad1908df3c9f157e1c2
I-amPrashant <airmax50cent@gmail.com>
2024-11-30 18:54:23 +0545

    4th commit: gsap 2 header animation add

--- Gsap 2/script.js
+++ Gsap 2/script.js
@@ -1,17 +1,25 @@
 
 const loaderWrapper=document.querySelector("#preloader-wrapper");
 const loader=document.querySelector("#loader");
+const headerItem=document.querySelectorAll(".header-item");
 document.addEventListener("DOMContentLoaded", ()=>{
     setTimeout(() => {
        const t1=gsap.timeline();
         t1.to(loader, {
-            scale:0,
-            duration:.7,
+            scale:2,
+            opacity:0,
+            duration:2,
             ease:'power2.out'
         }).to(loaderWrapper, {
             y:'-100%',
-            duration:.7,
+            duration:.9,
             ease:'power2.out',
-        }, '+.5')
-   }, 4000);
+        }, '+1').from(headerItem, {
+            x:'-50px',
+            opacity:0,
+            duration:.8,
+            ease:'back.out(4)',
+            stagger:.2,
+        })
+   }, 3500);
 })
