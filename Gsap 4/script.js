2ceca9b12939143b0c93cd15f091d1aa1f4af9cb
I-amPrashant <airmax50cent@gmail.com>
2025-06-01 12:22:34 +0545

    add simple button animation

--- Gsap 4/script.js
+++ Gsap 4/script.js
@@ -1,4 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     
-    
+    document.querySelector(".menu-icon").addEventListener("click", () => {
+        console.log('i ran')
+        document.querySelector(".menu-icon").classList.toggle("menu-active");   
+    });
 });
